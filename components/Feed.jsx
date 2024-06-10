1737782e3dac381f07735b65b5fd8f1f02cb7feb
Amit Singh <iamitsinghthakur123@gmail.com>
2024-06-10 12:08:14 +0530

    fixed prompt layout

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -6,7 +6,7 @@ import PromptCard from "./PromptCard";
 
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
-    <div className="mt-16">
+    <div className="mt-16 prompt_layout">
       {data.map((prompt) => (
         <PromptCard
           key={prompt._id}
